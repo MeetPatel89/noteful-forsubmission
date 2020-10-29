f1de477b96b69073752a85e4b1fdbb0d2bf830e6
Meet Patel <meetaiims@yahoo.com>
2020-10-29 18:34:04 -0400

    After revision

--- src/ValidateError.js
+++ src/ValidateError.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function ValidationError(props){
-    return (props.message) && <div>{props.message}</div>
+    return (props.message) ? <div>{props.message}</div> :
         
             
         
     
-    return <></>
+    <></>
 }
